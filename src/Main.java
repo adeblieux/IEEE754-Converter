7887806e4d8b729312eb3f5d789c671c0e10a51c
A. DeBlieux <113622999+adeblieux@users.noreply.github.com>
2024-06-05 14:34:08 -0500

    Update Main.java
    Signed-off-by: A. DeBlieux <113622999+adeblieux@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-package csc4101assignment2;
 import java.util.Scanner;
 /**
  *
  * @author ad
  */
-public class CSC4101Assignment2 {
-
-    public static void main(String[] args) {
-        
-        Scanner in = new Scanner(System.in);
-        Convertor.IEEE754(in.nextDouble());
-    }
+public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    Convertor.IEEE754(in.nextDouble());
 }
