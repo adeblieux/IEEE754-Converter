56d707497dca2a68fe9811b8c54aa4b6c89e65fb
A. DeBlieux <113622999+adeblieux@users.noreply.github.com>
2024-06-05 14:30:18 -0500

    Create Main.java
    Signed-off-by: A. DeBlieux <113622999+adeblieux@users.noreply.github.com>

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package csc4101assignment2;
+import java.util.Scanner;
+/**
+ *
+ * @author ad
+ */
+public class CSC4101Assignment2 {
+
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        Convertor.IEEE754(in.nextDouble());
+    }
+}
